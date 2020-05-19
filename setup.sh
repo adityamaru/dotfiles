9ae861a39b3c48a27832b0593f6ae51ebfa78ff4
Aditya Maru <adityamaru@gmail.com>
2020-05-19 13:12:55 -0400

    Added setup shell script

--- setup.sh
+++ setup.sh
@@ -5,10 +5,7 @@ ln -sf tmux/tmux.conf ~/.tmux.conf
 ln -sf vim/.vimrc ~/.vimrc
 ln -sf zsh/.zshrc ~/.zshrc
 
-# Create theme and plugins for zsh
-mkdir -p ~/.vim
-mkdir -p $ZSH_CUSTOM
-
+# Link theme and plugins for zsh
 ln -sf vim/colors ~/.vim/colors
 ln -sf zsh/plugins $ZSH_CUSTOM/plugins
 ln -sf zsh/themes $ZSH_CUSTOM/themes
